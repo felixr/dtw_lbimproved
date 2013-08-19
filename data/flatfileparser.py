ad3fd9f3adb72e6af0223d8dd34087b5e379131e
Felix Riedel <felix.riedel@gmail.com>
2013-08-19 17:09:44 +0200

    fixed python indentation, removed with= argument

--- data/flatfileparser.py
+++ data/flatfileparser.py
@@ -1,9 +1,7 @@
-
-
 def readFlat(filename, delimiter):
-  f = open(filename)
-  ans = []
-  for line in f:
-   ans.append(map(lambda x:float(x), filter(lambda x:len(x)>0,line.split(delimiter))))
-  return ans
+    f = open(filename)
+    ans = []
+    for line in f:
+     ans.append(map(lambda x:float(x), filter(lambda x:len(x)>0,line.split(delimiter))))
+    return ans
 
